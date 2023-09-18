c66167e128a8b2f5baf86ca7c2df5fefe0bf2b74
Sairam Chekuri <sairamc3@gmail.com>
2023-09-18 15:17:48 +0530

    Applying the solution for fist problem

--- basic_programming/src/main/java/org/example/FavouriteSinger.java
+++ basic_programming/src/main/java/org/example/FavouriteSinger.java
@@ -3,10 +3,9 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
+
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 /**
  * This class is to solve the problem in hacker earth portal
@@ -14,22 +13,46 @@ import java.util.stream.IntStream;
  */
 public class FavouriteSinger {
 
+    /**
+     * there is timing issue with this approach.
+     * Need to identify a better way to solve the problem
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int noOfCases = Integer.parseInt(br.readLine());
 
-        for(int i=0; i<noOfCases; i++){
+
             String[] stringArray = br.readLine().split(" ");
-            List<Integer> numbers = Arrays.stream(stringArray).map(Integer::parseInt).toList();
+            List<Long> numbers = Arrays.stream(stringArray).map(Long::parseLong).collect(Collectors.toList());
 
-            List<Integer> distinctValues = numbers.stream().distinct().toList();
+            List<Long> distinctValues = numbers.stream().distinct().collect(Collectors.toList());
 
-            //distinctValues.stream().map(dis -> numbers.stream().filter(Object::equals))
+            Map<Long, Long> noOfDistinct = new HashMap<>();
+
+            for( Long distinctValue: distinctValues){
+
+               noOfDistinct.put(distinctValue, numbers.stream().filter(distinctValue::equals).count());
+
+            }
+
+            Long max = Collections.max(noOfDistinct.values());
+
+             int result = 0;
+
+            for (Map.Entry<Long, Long> entry : noOfDistinct.entrySet()) {
+                if (entry.getValue().equals(max)) {
+                    result ++;
+                }
+            }
+
+            System.out.println(result);
 
 
 
-        }
+
 
     }
 }
